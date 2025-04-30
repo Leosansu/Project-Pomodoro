2c8d8f2741f1bc6f11f6e4a51d9d136f0d4185cb
Leonam <leosuerte_@hotmail.com>
2025-04-30 14:51:39 -0300

    Foi criada a parte inicial do formulário com o uso label e input.

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,30 @@ export function App() {
         <CountDown />
       </ContainerFluid>
 
+      <ContainerFluid>
+        <form className='form' action="">
+          <div className='formRow'>
+            <label htmlFor='meuInput'>tarefa</label>
+            <input id='meuInput' type='text' />
+
+          </div>
+
+          <div className='formRow'>
+
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </ContainerFluid>
+
     </>
   );
 
